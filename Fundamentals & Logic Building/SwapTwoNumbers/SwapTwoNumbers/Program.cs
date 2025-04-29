c9f7490ae7f12632e55ce59b1b4ddeaec6e17a87
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-04-29 10:58:14 +0930

    hello world and swap numbers

---
+++ Fundamentals & Logic Building/SwapTwoNumbers/SwapTwoNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int num1, num2, temp;
+Console.WriteLine("please enter the first number");
+if (!(int.TryParse(Console.ReadLine(), out num1)))
+{
+    exitEnvironment();
+}
+
+
+
+if (!(int.TryParse(Console.ReadLine(), out num2)))
+{
+    exitEnvironment();
+
+}
+
+Console.WriteLine($" Before swapping \n num1 = {num1}, num2 = {num2}");
+
+temp = num1;
+num1 = num2;
+num2= temp;
+
+Console.WriteLine($" After swapping \n num1 = {num1}, num2 = {num2}");
+
+void exitEnvironment()
+{
+    Console.WriteLine("Run program again and Please enter a valid number");
+    Environment.Exit(0);
+}
+
