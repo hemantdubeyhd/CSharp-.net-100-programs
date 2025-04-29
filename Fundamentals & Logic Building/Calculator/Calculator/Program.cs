a5319766670bf0a7745d17935ee731494e4f308c
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-04-29 16:24:50 +0930

    comments for future updates added

--- Fundamentals & Logic Building/Calculator/Calculator/Program.cs
+++ Fundamentals & Logic Building/Calculator/Calculator/Program.cs
@@ -4,5 +4,6 @@ Console.WriteLine("Hello, World!");
 //this simple program calcultes addition substration dicision and multiplication:
 //here we are asking for only two number for whatever operation user chose, not ideal but this is just to learn switch case.
 //in future on the basis of operation we can ask multple inputes for add, sub andmul but only two for div
-//Also in fiuture we can check if divident is zero
+//Also in fiuture we can check if divident is zero if person has enterd zero,
+//also can check if any of the number for multiplication is 0, warn user.
 
