f3915262009e2313fa689aba72f4db11b1fc07b1
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-04-29 16:21:25 +0930

    SimpleCalculator

---
+++ Fundamentals & Logic Building/Calculator/Calculator/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//this simple program calcultes addition substration dicision and multiplication:
+//here we are asking for only two number for whatever operation user chose, not ideal but this is just to learn switch case.
+//in future on the basis of operation we can ask multple inputes for add, sub andmul but only two for div
+//Also in fiuture we can check if divident is zero
+
--- Fundamentals & Logic Building/LeapYearCheck/LeapYearCheck/Program.cs
+++ Fundamentals & Logic Building/LeapYearCheck/LeapYearCheck/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LeapYearCheck;
+
 Console.WriteLine("Hello, World!");
 int yearToCheck = InputYearandCheck.EnterTheYear("Please eneter the year in yyyy formate");
 LeapYearChecker.CheckIfLeapYear(yearToCheck);
