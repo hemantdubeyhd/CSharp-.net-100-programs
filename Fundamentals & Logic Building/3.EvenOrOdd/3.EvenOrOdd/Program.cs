50b7d2f05b5c9c3fed8197a4ed203cfcb8bf57d3
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-04-29 11:34:58 +0930

    EvenOrOdd

---
+++ Fundamentals & Logic Building/3.EvenOrOdd/3.EvenOrOdd/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int num = GetValidNumber("Please enter a number");
+CheckEvenOrOdd(num);
+//Console.WriteLine(num);
+
+int GetValidNumber(string statement)
+{
+    Console.WriteLine(statement);
+    while (true)
+    {
+       
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int validNumber))
+        {
+            return validNumber;
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid number");
+        } 
+    }
+}
+
+void CheckEvenOrOdd(int num)
+{
+    if (num % 2 == 0)
+    {
+        Console.WriteLine("User has enterd an even number");
+    }
+    else
+    {
+        Console.WriteLine("User has entered a odd number");
+    }
+}
