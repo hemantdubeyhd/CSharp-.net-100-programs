567dfbd57d0a0bdc7f67b6d1498e20011626f4d8
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-05-02 15:13:47 +0930

    deleted some code from program.cs

--- Fundamentals & Logic Building/TableOfANumber/TableOfANumber/Program.cs
+++ Fundamentals & Logic Building/TableOfANumber/TableOfANumber/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using TableOfANumber;
-Console.WriteLine("Hello, World!");
 int number = InputHelper.GetAValidNumber("Please enter anumber");
-//Console.WriteLine($"number enterd: {number}");
 PrintTheTable.PrintTheTableOfANumber(number);
