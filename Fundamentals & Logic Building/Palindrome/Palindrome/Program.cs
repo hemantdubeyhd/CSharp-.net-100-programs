788bb3461a6be5ef7bb589ea2da630af80179da3
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-05-13 19:56:52 +0930

    deleted an unnecessary line.
    program can be improve more.
    few good point are it's modular and great effort naming are somewhat good.
    
    Need to focus on spelling always character jumbiling.

--- Fundamentals & Logic Building/Palindrome/Palindrome/Program.cs
+++ Fundamentals & Logic Building/Palindrome/Palindrome/Program.cs
@@ -3,5 +3,4 @@ using Palindrome;
 
 //We are here for the sake of just the logic learning, not checking if user has eneterd negative numebrs, negatiev numbers cant be palindrome.
 int numberToCheckIfPalindrom = InputHelper.GetAValidNumber("Eneter a number please");
-//Console.WriteLine(numberToCheckIfPalindrom);
 CheckingPalindrome.CheckIfPalindrome(numberToCheckIfPalindrom);
