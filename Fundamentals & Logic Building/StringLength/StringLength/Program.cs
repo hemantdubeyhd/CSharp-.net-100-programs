68614e89b3d378d6d0dba047585a66edfbf2e8ca
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-05-19 15:18:28 +0930

    Str lenght w/o .Length

--- Fundamentals & Logic Building/StringLength/StringLength/Program.cs
+++ Fundamentals & Logic Building/StringLength/StringLength/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using StringLength;
+
+
 string stringToCheckLenghtOf = InputHelper.receivingAString("Please Eneter a String");
 
 int lenthOftheString = FindStringLength.findingTheLenghtOfTheString(stringToCheckLenghtOf);
+
 Console.WriteLine(lenthOftheString);
