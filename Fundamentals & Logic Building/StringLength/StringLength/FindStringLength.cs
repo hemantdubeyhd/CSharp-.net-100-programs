c5587e7454dab012cd636005b84451c33138c12b
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-05-19 15:16:16 +0930

    length without .Lenght
    string length, easiest logic

---
+++ Fundamentals & Logic Building/StringLength/StringLength/FindStringLength.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLength
+{
+    internal static class FindStringLength
+    {
+        public static int findingTheLenghtOfTheString(string str)
+        {
+            int lenght = 0;
+            foreach (char c in str) 
+            {
+                lenght++;
+            }
+            return lenght;
+        }
+    }
+}
---
+++ Fundamentals & Logic Building/StringLength/StringLength/InputHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLength
+{
+    internal static class InputHelper
+    {
+       
+            public static string receivingAString(string statement)
+            {
+                Console.WriteLine(statement);
+                string str;
+                while (true)
+                {
+                    str = Console.ReadLine();
+                    if (string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str))
+                    {
+                        Console.WriteLine("Please enter a valid string");
+                    }
+                    else
+                    {
+                        return str;
+                    }
+                }
+            }
+        }
+    }
+
