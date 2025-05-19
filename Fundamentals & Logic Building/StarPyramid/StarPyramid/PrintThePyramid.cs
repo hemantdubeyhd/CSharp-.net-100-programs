dd5e5f052e8238898e124424c9795899272750a4
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-05-19 17:40:26 +0930

    pyramid of starts

---
+++ Fundamentals & Logic Building/StarPyramid/StarPyramid/PrintThePyramid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarPyramid
+{
+    internal static class PrintThePyramid
+    {
+      
+        public static void printingThePyramid(int noOfStorisToPrint)
+        {
+            int numberOfStars = 1;
+            int numberOfSpaceInTheLine = noOfStorisToPrint;
+            int k = 0;
+            for (int i = 0; i < noOfStorisToPrint; i++)
+            {
+                for(int j = 0; j < numberOfSpaceInTheLine; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (; k <= i; k++)
+                {
+                    Console.Write(" H");
+                }
+                Console.WriteLine("\n");
+
+                numberOfSpaceInTheLine--;
+                k = 0;
+            }
+        }
+    }
+}
---
+++ Fundamentals & Logic Building/StarPyramid/StarPyramid/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StarPyramid;
+int numberofStoriesinPyramid = InputHelper.validateTheInput("Please enter a number > 0");
+//Console.WriteLine(numberofStoriesinPyramid);
+PrintThePyramid.printingThePyramid(numberofStoriesinPyramid);
