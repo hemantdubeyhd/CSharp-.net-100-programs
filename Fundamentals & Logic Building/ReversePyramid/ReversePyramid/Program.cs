a10b8da2c82463217116015a36760097d01aa7ea
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-05-21 09:22:04 +0930

    reversedPyramid
    added inputhelper class

---
+++ Fundamentals & Logic Building/ReversePyramid/ReversePyramid/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReversePyramid;
+int input = InputHelper.validateTheInput("Please enetr a number");
+
+Console.WriteLine(input);
