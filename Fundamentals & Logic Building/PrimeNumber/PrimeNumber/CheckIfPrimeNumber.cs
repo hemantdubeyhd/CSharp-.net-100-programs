f0f5513b66f19eea29d6107cd138a07489f0a14c
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-05-21 14:11:53 +0930

    prime numer working fine

---
+++ Fundamentals & Logic Building/PrimeNumber/PrimeNumber/CheckIfPrimeNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal static class CheckIfPrimeNumber
+    {
+        public static void checkingIfNumberIsPrime(int numberToCheck)
+        {
+            for (int divisor = 2; divisor <= Math.Sqrt(numberToCheck); divisor++)
+            {
+                if((numberToCheck % divisor) == 0)
+                {
+                    Console.WriteLine($"{numberToCheck} Number is not a prime numebr");
+                    return;
+                }
+                
+            }
+            Console.WriteLine($"{numberToCheck} Number is a prime numebr");
+
+        }
+    }
+}
---
+++ Fundamentals & Logic Building/PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using PrimeNumber;
+
+int userInput = InputHelper.checkTheNumberIfPrime("Please eneter a number > 1");
+CheckIfPrimeNumber.checkingIfNumberIsPrime(userInput);
