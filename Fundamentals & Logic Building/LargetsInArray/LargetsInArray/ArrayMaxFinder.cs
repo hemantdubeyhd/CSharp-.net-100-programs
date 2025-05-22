f33a74e6020c6c5de81063028252907804d4e825
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-05-22 18:10:12 +0930

    corrected the logic

---
+++ Fundamentals & Logic Building/LargetsInArray/LargetsInArray/ArrayMaxFinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestInArray
+{
+    internal class ArrayMaxFinder
+    {
+        public static void findingTheLargestInArray(int[] array)
+        {
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                  max = array[i];
+                }
+            }
+           // ArrayPrinter.PrintingTheArray(array);
+            Console.WriteLine($"Largest Element in array is {max}");
+        }
+    }
+}
---
+++ Fundamentals & Logic Building/LargetsInArray/LargetsInArray/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using LargestInArray;
+
+int sizeOfArray = InputHelper.ValidateTheSizeOfArray("Please enter the size of the array!");
+int[] intArray = new int[sizeOfArray];
+
+intArray = InputHelper.addingValuesToArrayAndValidation(intArray);
+ArrayPrinter.PrintingTheArray(intArray);
+ArrayMaxFinder.findingTheLargestInArray(intArray);
