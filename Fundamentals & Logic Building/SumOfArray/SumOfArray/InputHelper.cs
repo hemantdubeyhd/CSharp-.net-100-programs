c6a676382d7617f28cfc88dd7b788875bf5ca397
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-05-22 12:01:20 +0930

    Sum of array
    Added InputHelper Class

---
+++ Fundamentals & Logic Building/SumOfArray/SumOfArray/InputHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfArray
+{
+    internal static class InputHelper
+    {
+        public static int ValidateTheSizeOFarray(string statement)
+        {
+            Console.WriteLine(statement);
+
+            while (true) 
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out var arraySize))
+                {
+                    if (arraySize <= 0)
+                    {
+                        Console.WriteLine($"You enetred the size of the array to be size {arraySize}, Pleasse enter a valid size");
+                        continue;
+                    }
+                    return arraySize;
+                }
+                else
+                { 
+                    Console.WriteLine("Please eneter a valid size of the array");
+                }
+            }
+        }
+    }
+}
---
+++ Fundamentals & Logic Building/SumOfArray/SumOfArray/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using SumOfArray;
+
+int sizeOfArray = InputHelper.ValidateTheSizeOFarray("Please enter the size of the array!");
+Console.WriteLine(sizeOfArray);
