da6d07543217ec89209238f1c4fefe119302a5a6
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-05-23 11:34:25 +0930

    bubbleSortArray
    simplest basic logic implemented

---
+++ Fundamentals & Logic Building/ArraySort/ArraySort/ArrayBubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySort
+{
+    internal class ArrayBubbleSort
+    {
+        public static void BubbleSortingAnArray(int[] array)
+        {
+            int temp;
+            for (int i = 0; i < array.Length -1; i++)
+            {
+                for (int j=0; j < array.Length - i -1; j++)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
+
+                }
+            }
+            
+            ArrayPrinter.PrintingTheArray(array);
+            //Console.WriteLine($"Largest Element in array is {max}");
+        }
+    }
+   
+}
---
+++ Fundamentals & Logic Building/ArraySort/ArraySort/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArraySort;
+
+int sizeOfArray = InputHelper.ValidateTheSizeOfArray("Please enter the size of the array!");
+int[] intArray = new int[sizeOfArray];
+
+intArray = InputHelper.addingValuesToArrayAndValidation(intArray);
+
+
+ArrayPrinter.PrintingTheArray(intArray);
+Console.WriteLine();
+
+ArrayBubbleSort.BubbleSortingAnArray(intArray);
