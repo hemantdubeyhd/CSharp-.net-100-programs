61fea47d69f856ecf6acbdb6df670ff286ca8d44
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-05-26 09:28:44 +0930

    Mertge two array

---
+++ Fundamentals & Logic Building/ArrayMerger/ArrayMerger/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using ArrayMerger;
+
+int size = InputHelper.ValidateTheSizeOfArray("Please Enter the size of the first array");
+int[] array1 = new int[size];
+
+Console.WriteLine("Please eneter the elements of first Array");
+InputHelper.addingValuesToArray(array1);
+
+
+size = InputHelper.ValidateTheSizeOfArray("Please Enter the size of the second array");
+int[] array2 = new int[size];
+
+Console.WriteLine("Please eneter the elements of first Array");
+InputHelper.addingValuesToArray(array2);
+
+int[] mergedArray = new int[size + array1.Length];
+
+//Console.WriteLine($"Lenght of merged array is {mergedArray.Length}");
