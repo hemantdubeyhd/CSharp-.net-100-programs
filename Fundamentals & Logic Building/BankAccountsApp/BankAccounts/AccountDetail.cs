d6a8b6f89066a61c4a0738c2cf29709dc52fe032
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-05-26 16:36:10 +0930

    classes added

---
+++ Fundamentals & Logic Building/BankAccountsApp/BankAccounts/AccountDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    internal class AccountDetail
+    {
+        AccountHolder accountHolder;
+        AccountBalance Accountbalance;
+    }
+}
---
+++ Fundamentals & Logic Building/BankAccountsApp/BankAccounts/AccountHolder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    internal class AccountHolder
+    {
+        public string Id {  get; set; }
+        public string Name {  get; set; }
+        public string EmailId {  get; set; }
+    }
+}
