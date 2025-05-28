381dc4fef85cf0d43e87db3838660900e065fb4d
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-05-28 15:26:20 +0930

    code added for calrtiy

--- Fundamentals & Logic Building/AddressBook/AddressBook/obj/Debug/net8.0/AddressBook.AssemblyInfo.cs
+++ Fundamentals & Logic Building/AddressBook/AddressBook/obj/Debug/net8.0/AddressBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ecf086da982110b669bb27fab4e913e81a155ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01638d5213f9f36009ef9a63df42fe5e776997f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
