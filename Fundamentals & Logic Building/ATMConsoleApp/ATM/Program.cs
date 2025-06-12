cccda34339ee0d2a3a1eb40b46040a61b987a77a
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-06-12 12:18:33 +0930

    created thye structure for different cases
    Need to analyse if these nested ifs and nested switch in it is the good structure or not?

--- Fundamentals & Logic Building/ATMConsoleApp/ATM/Program.cs
+++ Fundamentals & Logic Building/ATMConsoleApp/ATM/Program.cs
@@ -23,11 +23,25 @@ while (true)
 
             Console.ReadLine();
         }
-        else {
-            if(choice == 4)
+        else
+        {
+            switch (choice)
             {
-                Environment.Exit(0);
+                case 1:
+                    Console.WriteLine("Case 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Case 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Closing the app and logging off....Thanks!! ");
+                    Environment.Exit(0);
+                    break;
             }
+          
         }
     }
     else
