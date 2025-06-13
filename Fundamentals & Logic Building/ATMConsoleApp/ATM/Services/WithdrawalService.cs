4a2979976657d3e701c9e0b831942e5afa4b029b
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-06-13 15:04:31 +0930

    first implementation of withdrawal.
    checked if balance is $0,
    Checked if amount to be withdaw is not greatr than amaount availbale.

--- Fundamentals & Logic Building/ATMConsoleApp/ATM/Services/WithdrawalService.cs
+++ Fundamentals & Logic Building/ATMConsoleApp/ATM/Services/WithdrawalService.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using ATM.Helpers;
+using ATM.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +12,27 @@ namespace ATM.Services
 {
     internal class WithdrawalService
     {
+        public static void Withdrawal(Account account)
+        {
+            if (account.Balance == 0)
+            {
+                Console.WriteLine($"You cant make an withdrawal, your current balacne is ${account.Balance}");
+                // return;
+            }
+            else
+            {
 
+                decimal amount = InputHelper.ValidateTheInput("Please enter the amount you want to withdraw");
+                if (account.Balance - amount < 0)
+                {
+                    Console.WriteLine($"You cant make an withdrawal, Withdrawal amount ${amount} is greater than amount available ${account.Balance}");
+                }
+                else
+                {
+                    account.Balance -= amount;
+                    Console.WriteLine($"Successg=fully withdrawn ${amount}, your current balance is {account.Balance}");
+                }
+            } 
+        }
     }
 }
