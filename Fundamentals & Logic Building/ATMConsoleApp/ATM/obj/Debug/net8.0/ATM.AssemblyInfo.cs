a9d2091a3d0c547d6ff87ca4e0cb2584814b36b0
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-06-13 10:50:11 +0930

    Added services classes
    not implemented yes, thought the idea of implementing services separately, rather one service class.

--- Fundamentals & Logic Building/ATMConsoleApp/ATM/obj/Debug/net8.0/ATM.AssemblyInfo.cs
+++ Fundamentals & Logic Building/ATMConsoleApp/ATM/obj/Debug/net8.0/ATM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e3ff1d826b75e024a55b564ec339116ffc9a91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+265762076e15a130c60c09540d9937d6acf024c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
