72c97714a7e8bd61e74cff20fedcc43703155e31
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-06-17 15:05:11 +0930

    Inheritance demo, defined the base classs

---
+++ Oop_Collections_File IO_LINQ/InheritanceDemoApp/InheritanceDemoApp/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemoApp
+{
+    internal class Person
+    {
+        private int _id;
+        public int Id 
+        {
+            
+            get => _id;
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException("Id must be greater than 0");
+                }
+            }
+        }
+        public string name { get; set; }
+    }
+}
