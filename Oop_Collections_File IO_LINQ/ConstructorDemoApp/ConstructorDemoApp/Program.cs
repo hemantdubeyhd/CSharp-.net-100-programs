ada2f61c48b4ecc7b474b7db7fc1606d03bcaf96
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-06-17 10:42:08 +0930

    Constructor Demo
    definitely not a great example, need cleaning

---
+++ Oop_Collections_File IO_LINQ/ConstructorDemoApp/ConstructorDemoApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConstructorDemoApp;
+
+Console.WriteLine("Hello, World!");
+Student student1 = new Student();
+Console.ReadKey();
+Student student2 = new Student(1, "Qays");
+Console.ReadKey();
+student2.displayStudentInfo(student2);
---
+++ Oop_Collections_File IO_LINQ/ConstructorDemoApp/ConstructorDemoApp/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDemoApp
+{
+    internal class Student
+    {
+        private int _id;
+        private string _firstName;
+
+        public Student()
+        {
+            Console.WriteLine("default Constuctur called");
+            _id = 0;
+            _firstName = "Unknown";
+        }
+        public Student(int id, string name)
+        {
+            _id = id;
+            _firstName = name;
+        }
+
+        public void displayStudentInfo(Student student)
+        {
+            Console.WriteLine($"Student id: {student._id}, Name: {student._firstName}");
+        }
+    }
+}
