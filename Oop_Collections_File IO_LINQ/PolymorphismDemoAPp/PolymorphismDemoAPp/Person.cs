66bf84cad7d0bee1502c76b38e92691141ce7502
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-06-18 12:21:01 +0930

    polymorph demo app
    added base class

---
+++ Oop_Collections_File IO_LINQ/PolymorphismDemoAPp/PolymorphismDemoAPp/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismDemoAPp
+{
+    internal class Person
+    {
+        private int _id;
+        private string _name;
+
+        public int Id { 
+            get 
+            { 
+                return _id; 
+            } 
+            set 
+            { 
+                if(value <= 0)
+                {
+                    throw new ArgumentException("Id must be grater than 0");
+                }
+                _id = value;
+            } 
+        }
+
+        public string Name
+        {
+            get
+            { 
+                return _name; 
+            }
+            set 
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null, empty or whotespaces!!");
+                }
+                _name = value;
+            }
+        }
+
+    }
+}
